9fd50dd797ca005cde1f99437848a457cf812cad
PeterFWS <sfw811@hotmail.com>
2020-06-03 13:58:27 +0200

    add git ignore and re-formulate documents

--- include/Sim3Solver.h
+++ include/Sim3Solver.h
@@ -18,7 +18,6 @@
 * along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #ifndef SIM3SOLVER_H
 #define SIM3SOLVER_H
 
@@ -27,107 +26,99 @@
 
 #include "KeyFrame.h"
 
-
-
 namespace ORB_SLAM2
 {
 
-class Sim3Solver
-{
-public:
+    class Sim3Solver
+    {
+    public:
+        Sim3Solver(KeyFrame *pKF1, KeyFrame *pKF2, const std::vector<MapPoint *> &vpMatched12, const bool bFixScale = true);
 
-    Sim3Solver(KeyFrame* pKF1, KeyFrame* pKF2, const std::vector<MapPoint*> &vpMatched12, const bool bFixScale = true);
+        void SetRansacParameters(double probability = 0.99, int minInliers = 6, int maxIterations = 300);
 
-    void SetRansacParameters(double probability = 0.99, int minInliers = 6 , int maxIterations = 300);
+        cv::Mat find(std::vector<bool> &vbInliers12, int &nInliers);
 
-    cv::Mat find(std::vector<bool> &vbInliers12, int &nInliers);
+        cv::Mat iterate(int nIterations, bool &bNoMore, std::vector<bool> &vbInliers, int &nInliers);
 
-    cv::Mat iterate(int nIterations, bool &bNoMore, std::vector<bool> &vbInliers, int &nInliers);
+        cv::Mat GetEstimatedRotation();
+        cv::Mat GetEstimatedTranslation();
+        float GetEstimatedScale();
 
-    cv::Mat GetEstimatedRotation();
-    cv::Mat GetEstimatedTranslation();
-    float GetEstimatedScale();
+    protected:
+        void ComputeCentroid(cv::Mat &P, cv::Mat &Pr, cv::Mat &C);
 
+        void ComputeSim3(cv::Mat &P1, cv::Mat &P2);
 
-protected:
+        void CheckInliers();
 
-    void ComputeCentroid(cv::Mat &P, cv::Mat &Pr, cv::Mat &C);
+        void Project(const std::vector<cv::Mat> &vP3Dw, std::vector<cv::Mat> &vP2D, cv::Mat Tcw, cv::Mat K);
+        void FromCameraToImage(const std::vector<cv::Mat> &vP3Dc, std::vector<cv::Mat> &vP2D, cv::Mat K);
 
-    void ComputeSim3(cv::Mat &P1, cv::Mat &P2);
+    protected:
+        // KeyFrames and matches
+        KeyFrame *mpKF1;
+        KeyFrame *mpKF2;
 
-    void CheckInliers();
+        std::vector<cv::Mat> mvX3Dc1;
+        std::vector<cv::Mat> mvX3Dc2;
+        std::vector<MapPoint *> mvpMapPoints1;
+        std::vector<MapPoint *> mvpMapPoints2;
+        std::vector<MapPoint *> mvpMatches12;
+        std::vector<size_t> mvnIndices1;
+        std::vector<size_t> mvSigmaSquare1;
+        std::vector<size_t> mvSigmaSquare2;
+        std::vector<size_t> mvnMaxError1;
+        std::vector<size_t> mvnMaxError2;
 
-    void Project(const std::vector<cv::Mat> &vP3Dw, std::vector<cv::Mat> &vP2D, cv::Mat Tcw, cv::Mat K);
-    void FromCameraToImage(const std::vector<cv::Mat> &vP3Dc, std::vector<cv::Mat> &vP2D, cv::Mat K);
+        int N;
+        int mN1;
 
+        // Current Estimation
+        cv::Mat mR12i;
+        cv::Mat mt12i;
+        float ms12i;
+        cv::Mat mT12i;
+        cv::Mat mT21i;
+        std::vector<bool> mvbInliersi;
+        int mnInliersi;
 
-protected:
+        // Current Ransac State
+        int mnIterations;
+        std::vector<bool> mvbBestInliers;
+        int mnBestInliers;
+        cv::Mat mBestT12;
+        cv::Mat mBestRotation;
+        cv::Mat mBestTranslation;
+        float mBestScale;
 
-    // KeyFrames and matches
-    KeyFrame* mpKF1;
-    KeyFrame* mpKF2;
+        // Scale is fixed to 1 in the stereo/RGBD case
+        bool mbFixScale;
 
-    std::vector<cv::Mat> mvX3Dc1;
-    std::vector<cv::Mat> mvX3Dc2;
-    std::vector<MapPoint*> mvpMapPoints1;
-    std::vector<MapPoint*> mvpMapPoints2;
-    std::vector<MapPoint*> mvpMatches12;
-    std::vector<size_t> mvnIndices1;
-    std::vector<size_t> mvSigmaSquare1;
-    std::vector<size_t> mvSigmaSquare2;
-    std::vector<size_t> mvnMaxError1;
-    std::vector<size_t> mvnMaxError2;
+        // Indices for random selection
+        std::vector<size_t> mvAllIndices;
 
-    int N;
-    int mN1;
+        // Projections
+        std::vector<cv::Mat> mvP1im1;
+        std::vector<cv::Mat> mvP2im2;
 
-    // Current Estimation
-    cv::Mat mR12i;
-    cv::Mat mt12i;
-    float ms12i;
-    cv::Mat mT12i;
-    cv::Mat mT21i;
-    std::vector<bool> mvbInliersi;
-    int mnInliersi;
+        // RANSAC probability
+        double mRansacProb;
 
-    // Current Ransac State
-    int mnIterations;
-    std::vector<bool> mvbBestInliers;
-    int mnBestInliers;
-    cv::Mat mBestT12;
-    cv::Mat mBestRotation;
-    cv::Mat mBestTranslation;
-    float mBestScale;
+        // RANSAC min inliers
+        int mRansacMinInliers;
 
-    // Scale is fixed to 1 in the stereo/RGBD case
-    bool mbFixScale;
+        // RANSAC max iterations
+        int mRansacMaxIts;
 
-    // Indices for random selection
-    std::vector<size_t> mvAllIndices;
+        // Threshold inlier/outlier. e = dist(Pi,T_ij*Pj)^2 < 5.991*mSigma2
+        float mTh;
+        float mSigma2;
 
-    // Projections
-    std::vector<cv::Mat> mvP1im1;
-    std::vector<cv::Mat> mvP2im2;
+        // Calibration
+        cv::Mat mK1;
+        cv::Mat mK2;
+    };
 
-    // RANSAC probability
-    double mRansacProb;
-
-    // RANSAC min inliers
-    int mRansacMinInliers;
-
-    // RANSAC max iterations
-    int mRansacMaxIts;
-
-    // Threshold inlier/outlier. e = dist(Pi,T_ij*Pj)^2 < 5.991*mSigma2
-    float mTh;
-    float mSigma2;
-
-    // Calibration
-    cv::Mat mK1;
-    cv::Mat mK2;
-
-};
-
-} //namespace ORB_SLAM
+} // namespace ORB_SLAM2
 
 #endif // SIM3SOLVER_H
